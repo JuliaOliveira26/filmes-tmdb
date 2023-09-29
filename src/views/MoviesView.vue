7b6bbfed7df59b47a05e9f2c103be65ae764adc0
JuliaOliveira26 <julinhalves200@gmail.com>
2023-09-29 11:53:58 -0300

    rtr

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,13 +1,18 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import api from '@/plugins/axios'
+import Loading from 'vue-loading-overlay'
 
+const isLoading = ref(false);
 const genres = ref([])
 const movies = ref([]);
+const getGenreName = (id) => genres.value.find((genre) => genre.id === id).name
+const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
 
 
 
 const listMovies = async (genreId) => {
+  isLoading.value = true;
     const response = await api.get('discover/movie', {
         params: {
             with_genres: genreId,
@@ -15,6 +20,7 @@ const listMovies = async (genreId) => {
         }
     });
     movies.value = response.data.results
+    isLoading.value = false;
 };
 onMounted(async () => {
   const response = await api.get('genre/movie/list?language=pt-BR')
@@ -22,8 +28,10 @@ onMounted(async () => {
 })
 </script>
 <template>
+  
   <h1>Filmes</h1>
   <ul class="genre-list">
+    <loading v-model:active="isLoading" is-full-page />
     <li v-for="genre in genres" :key="genre.id" @click="listMovies(genre.id)" class="genre-item">
     {{ genre.name }}
 </li>
@@ -34,10 +42,15 @@ onMounted(async () => {
     <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
     <div class="movie-details">
       <p class="movie-title">{{ movie.title }}</p>
-      <p class="movie-release-date">{{ movie.release_date }}</p>
-      <p class="movie-genres">{{ movie.genre_ids }}</p>
+      <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
+      <p class="movie-genres">
+  <span v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)">
+    {{ getGenreName(genre_id) }} 
+  </span>
+  
+</p>
     </div>
-    
+  
   </div>
 </div>
 </template>
@@ -104,4 +117,26 @@ onMounted(async () => {
   list-style: none;
   margin-bottom: 2rem;
 }
+.movie-genres {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.2rem;
+}
+
+.movie-genres span {
+  background-color: #748708;
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+
+.movie-genres span:hover {
+  cursor: pointer;
+  background-color: #455a08;
+  box-shadow: 0 0 0.5rem #748708;}
 </style>
--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -1,23 +1,61 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import api from '@/plugins/axios'
+import Loading from 'vue-loading-overlay'
 
-const genres = ref([])
+const isLoading = ref(false);
+const Genres = ref([])
+const programaMovies = ref([]);
+const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
+const getGenresName = (id) => genres.value.find((genre) => genre.id === id).name
 
+
+
+const listTv = async (genreId) => {
+  isLoading.value = true;
+    const response = await api.get('discover/movie', {
+        params: {
+            with_genres: genreId,
+            language: 'pt-BR'
+        }
+    });
+    programaMovies.value = response.data.results
+    isLoading.value = false
+};
 onMounted(async () => {
-  const response = await api.get('genre/tv/list?language=pt-BR')
-  genres.value = response.data.genres
+  const response = await api.get('genre/movie/list?language=pt-BR')
+  Genres.value = response.data.genres
 })
 </script>
-
 <template>
-  <h1>Programas de TV</h1>
+  <h1>Filmes</h1>
   <ul class="genre-list">
-    <li v-for="genre in genres" :key="genre.id" class="genre-item">
-        {{ genre.name }}
-    </li>
+    <li v-for="genre in Genres" :key="genre.id" @click="listTv(genre.id)" class="genre-item">
+    {{ genre.name }}
+</li>
   </ul>
+  <loading v-model:active="isLoading" is-full-page />
+
+  <div class="movie-list">
+  <div v-for="programaMovie in programaMovies" :key="programaMovie.id" class="movie-card">
+    
+    <img :src="`https://image.tmdb.org/t/p/w500${programaMovie.poster_path}`" :alt="programaMovie.name" />
+    <div class="tv-details">
+      <p class="tv-title">{{ programaMovie.original_name }}</p>
+      <p class="tv-release-date">{{ programaMovie.first_air_date }}</p>
+      <p class="tv-release-date">{{ formatDate(programaMovie.release_date) }}</p>
+      <p class="tv-genres">
+
+  <span v-for="genre_id in programaMovie.genre_ids" :key="genre_id" @click="listTv(genre_id)">
+    {{ getGenresName(genre_id) }} 
+  </span>
+</p>
+    </div>
+    
+  </div>
+</div>
 </template>
+
 
 <style scoped>
 .genre-list {
@@ -30,18 +68,79 @@ onMounted(async () => {
 }
 
 .genre-item {
-  background-color: #5d6424;
+  background-color: #387250;
   border-radius: 1rem;
   padding: 0.5rem 1rem;
-  align-self: center;
   color: #fff;
-  display: flex;
-  justify-content: center;
 }
 
 .genre-item:hover {
   cursor: pointer;
-  background-color: #7d8a2e;
-  box-shadow: 0 0 0.5rem #5d6424;
+  background-color: #4e9e5f;
+  box-shadow: 0 0 0.5rem #387250;
 }
+
+.movie-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.movie-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.movie-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-details {
+  padding: 0 0.5rem;
+}
+
+.tv-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
+}
+.movie-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  margin-bottom: 2rem;
+}
+.tv-genres {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.2rem;
+}
+
+.tv-genres span {
+  background-color: #748708;
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+
+.tv-genres span:hover {
+  cursor: pointer;
+  background-color: #455a08;
+  box-shadow: 0 0 0.5rem #748708;
+}
+
 </style>
